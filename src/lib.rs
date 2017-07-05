af728df1bcc58a8dfe901aff412dcd083cdeadfe
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2017-07-05 18:46:11 -0400

    Add extern function to the best of my meager ability

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,30 @@ pub mod ast;
 mod tests;
 
 extern crate regex;
+extern crate libc;
+
+use libc::{c_char};
+use std::fs::File;
+use std::io::Read;
+use std::ffi::CStr;
+use ast::GraphQLType;
+
+#[no_mangle]
+#[allow(unused)]
+pub extern fn parse(s: *const c_char) -> Vec<GraphQLType> {
+    let c_str = unsafe {
+        assert!(!s.is_null());
+
+        CStr::from_ptr(s)
+    };
+
+    let filename = c_str.to_str().unwrap();
+
+    let mut file = File::open(filename).expect("Unable to open file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents);
+    gqlidl::parse_schema(contents.as_str()).unwrap()
+}
+
+#[allow(dead_code)]
+pub extern fn fix_linking_when_not_using_stdlib() { panic!() }
